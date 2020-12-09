9982e92e7aa4ae85c9eb4ab209fbb329baedbff3
trzytematyczna <monikrakoczy@gmail.com>
2020-12-09 19:10:55 +0100

    day 9 solution 2

--- day9/xmas-encrypted.py
+++ day9/xmas-encrypted.py
@@ -12,10 +12,17 @@ def checksum(relative_start,relative_end,num):
                 return True
     return False
 
-
+wrong = 0
 for i in range(preamble,len(codes)):
     if not checksum((i-preamble),(i-1),codes[i]):
-        print(codes[i])
+        wrong = codes[i]
 
 
-#85848519
+# 85848519
+
+for start in range(0,len(codes)):
+    for step in range(2, len(codes)+1):
+        if sum(codes[start:step])==wrong and codes[start:step]!=[wrong]:
+            print("contiguous set of at least two numbers:",codes[start:step])
+            print(min(codes[start:step])+max(codes[start:step]))
+# 13414198
